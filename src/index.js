decd8781a9c1a1ce19e44f097a1a94fd1c8c0cc5
Mratyunjay <tripathi.mratyunjay.1@gmail.com>
2020-11-27 11:01:47 +0530

    first

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
-}
 
-module.exports = httpServer;
+    if(req.url=="/welcome"){
+        // console.log(res);
+        res.statuCode=200;
+        res.setHeader('content-type','text/plain');
+        res.write("Welcome to Dominos!");
+        res.end();
+    
+    }else if(req.url=="/contact"){
+        const body={
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+            };
+
+        res.statusCode=200;
+        res.setHeader('content-type','application/json');
+        res.write(JSON.stringify(body));
+            res.end();
+    }else{
+        res.statuCode=404;
+        res.end();
+    }
+    
+}
+httpServer.listen(8081);
+
+// module.exports = httpServer;
